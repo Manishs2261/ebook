960250d404f6c5e976e1619dd9c490c624be18a1
Manish sahu <sanjusahu7326@gmail.com>
2024-09-15 15:57:59 +0530

    add hadh password

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -2,6 +2,7 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModels from "./userModels";
+import bcrypt from "bcrypt";
 
 const createUser = async (
   req: Request,
@@ -22,11 +23,17 @@ const createUser = async (
 
     //data base call
     const user = await userModels.findOne({email:email});
-    
+
  if(user){
     const error = createHttpError(400,"User already exists with this email");
     return next(error);
  }
+
+ //password hashing
+
+
+ const hashedPassword = await bcrypt.hash(password,10);
+ 
     //process
 
 
