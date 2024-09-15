37962cde40439cecd542d30dde0164bac784b001
Manish sahu <sanjusahu7326@gmail.com>
2024-09-15 15:47:12 +0530

    add user models

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,6 +1,7 @@
 
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
+import userModels from "./userModels";
 
 const createUser = async (
   req: Request,
@@ -19,6 +20,13 @@ const createUser = async (
         return next(error);
     }
 
+    //data base call
+    const user = await userModels.findOne({email:email});
+    
+ if(user){
+    const error = createHttpError(400,"User already exists with this email");
+    return next(error);
+ }
     //process
 
 
